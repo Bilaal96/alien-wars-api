226e88716f3404116ca866bc25244a8e0f6ff9b9
wainhouse <lukewainhouse@hotmail.com>
2023-05-02 17:27:23 +0100

    LW: This commit comtains the char create for human and alien

---
+++ controllers/characterController.js
@@ -0,0 +1,33 @@
+import Character from "../models/Character.js";
+
+export async function createCharacter(req, res, next) {
+  console.log(req.body);
+  console.log(req.session);
+  const { race, characterName, username } = req.body;
+  try {
+    if (race === "human") {
+      // set a human with the following properties
+      const character = await Character.create({
+        race,
+        username: username,
+        characterName: characterName,
+        attack: 10,
+        defense: 11,
+        gold: 100,
+      });
+      res.status(201).json({ character });
+    } else if (race === "alien") {
+      const character = await Character.create({
+        race,
+        username: username,
+        characterName: characterName,
+        attack: 11,
+        defense: 10,
+        gold: 100,
+      });
+      res.status(201).json({ character });
+    }
+  } catch (err) {
+    next(err);
+  }
+}
